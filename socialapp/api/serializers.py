6da37974f8b8d185f1d9435dc60df6868b25b079
Tufan <66652016+Itsbiggertheinside@users.noreply.github.com>
2021-02-28 22:00:57 +0300

    follow serializer

--- socialapp/api/serializers.py
+++ socialapp/api/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
-from .models import Profile, Post
+from .models import Profile, Post, Follow
 
+
+
+class FollowSerializer(serializers.ModelSerializer):
+
+    followed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Follow
+        exclude = ('id', 'follower',)
+
+    def get_followed(self, instance):
+        return list(instance.followed.values_list('slug', flat=True))
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,6 +29,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
     picture = serializers.ImageField(read_only=True)
     post_set = PostSerializer(read_only=True, many=True)
+    follow_set = FollowSerializer(read_only=True, many=True)
 
     class Meta:
         model = Profile
