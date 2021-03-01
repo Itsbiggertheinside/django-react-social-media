c0287ff189915956fee0cf144ee0536e175c68a8
Tufan <66652016+Itsbiggertheinside@users.noreply.github.com>
2021-03-01 18:40:27 +0300

    post filtering

--- socialapp/api/views.py
+++ socialapp/api/views.py
@@ -11,11 +11,17 @@ class ProfileViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.
     lookup_field = 'slug'
 
 
-class PostViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
-    queryset = Post.objects.all()
+class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = (IsOwnerOrReadOnly, )
     lookup_field = 'slug'
+
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(user__slug=username)
+        return queryset
     
     def perform_create(self, serializer):
         profile = self.request.user.profile
