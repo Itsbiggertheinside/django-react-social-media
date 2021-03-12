13f3521b461e1d4bd87e1c7809d07de893781731
Tufan <66652016+Itsbiggertheinside@users.noreply.github.com>
2021-03-12 23:16:20 +0300

    started template

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="home">
-    <h1 class="text_primary">Homepage</h1>
+    <b-container fluid>
+      <b-row>
+        <b-col sm='0' md='4'>
+          <LeftMenu />
+        </b-col>
+        <b-col>
+          <h1>Content</h1>
+        </b-col>
+        <b-col sm='0' md='4'>
+          <RightMenu />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import LeftMenu from '../components/home/LeftMenu';
+import LeftMenu from '../components/home/RightMenu';
 
 export default {
-
+  components: {
+    LeftMenu, RightMenu
+  }
 };
 
 </script>
